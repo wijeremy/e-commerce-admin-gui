d651031ffda0cb3acd18fab4d7f689b97e34c0c2
javier vilchis <javivilchis@gmail.com>
2021-08-10 18:50:37 -0500

    adding a whole lot of stuff

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,117 @@
+const router = require('express').Router();
+const jwt = require("jsonwebtoken");
+const { User } = require('../../models');
+const bcrypt = require('bcrypt');
+const auth = require("../../utils/auth");
+require("dotenv").config();
+// Register
+router.post("/register", async (req, res) => {
+   
+     // Our register logic starts here
+     try {
+       // Get user input
+       const { first_name, last_name, email, password } = req.body;
+   
+       // Validate user input
+       if (!(email && password && first_name && last_name)) {
+         res.status(400).send("All input is required");
+       }
+   
+       // check if user already exist
+       // Validate if user exist in our database
+       const oldUser = await User.findOne( {where: { password }});
+
+       if (oldUser) {
+         return res.status(409).send("User Already Exist. Please Login");
+       }
+
+       //Encrypt user password
+       encryptedPassword = await bcrypt.hash(password, 10);
+   
+       // Create user in our database
+       const user = await User.create({
+         first_name,
+         last_name,
+         email: email.toLowerCase(), // sanitize: convert email to lowercase
+         password: encryptedPassword,
+       });
+   
+       // Create token
+       const token = jwt.sign(
+         { user_id: user._id, email },
+         process.env.TOKEN_KEY,
+         {
+           expiresIn: "2h",
+         }
+       );
+       // save user token
+       user.token = token;
+   
+       // return new user
+       res.status(201).json(user);
+     } catch (err) {
+      res.json(err.message);
+     }
+     // Our register logic ends here
+   });
+
+router.post("/post",auth, async (req, res) => {
+     console.log("no here!");
+     
+     res.json("hey");
+     // jwt.verify(req.token, process.env.TOKEN_KEY, (err, authData) => {
+     //      if(err) {
+     //           res.sendStatus(403);
+     //      } else {
+     //           res.json({
+     //                message: 'Post created...',
+     //                authData
+     //            });
+     //      }
+     // });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+     // Our login logic starts here
+     try {
+       // Get user input
+       const { email, password } = req.body;
+  
+       // Validate user input
+       if (!(email && password)) {
+         res.status(400).send("All input is required");
+       }
+       // Validate if user exist in our database
+       const user = await User.findOne({ where: { email }});
+       //const userdata = user.map((username) => username.get({ plain: true }));
+  
+       if (user && (await bcrypt.compare(password, user.password))) {
+         // Create token
+         const token = jwt.sign( { 
+              id: user.id, email 
+          }, 
+          process.env.TOKEN_KEY,{expiresIn: "2h"});
+   
+         // save user token
+         user.token = token;
+     res.json(user.token);
+         // user
+         //res.status(200).json(user);
+       } else {
+          res.status(400).send("Invalid Credentials");
+       }
+
+     } catch (err) {
+       console.log(err);
+     }
+     // Our register logic ends here
+   });
+
+
+
+router.get("/welcome", auth, (req, res) => {
+     res.status(200).send("Welcome 🙌 ");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+//add JWT access token here.
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
