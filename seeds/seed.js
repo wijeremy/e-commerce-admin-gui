61705dbb19542a9cb52b885e6e0b17bfd8aa825c
wijeremy <jeremydavwilliams@gmail.com>
2021-08-10 19:40:12 -0500

    got the user cart items model

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,10 +2,15 @@ const sequelize = require('../config/connection');
 const { User, Product } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+// const projectData = require('./projectData.json');
 
 const seedDatabase = async () => {
 	await sequelize.sync({ force: true });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
 
 	process.exit(0);
 };
