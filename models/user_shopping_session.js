8c15f7bbfebb01dc04f47b82192e61c165b71fbf
jamesj995 <mrjam7@gmail.com>
2021-08-10 19:49:44 -0500

    added user_shopping_session

---
+++ models/user_shopping_session.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class UserShoppingSession extends Model {}
+
+UserShoppingSession.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    total: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'usershoppingsession',
+  }
+);
+
+module.exports = UserShoppingSession;
