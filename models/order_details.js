c2843cfff909e9b2b130473eb44ff9ae76be94cd
jamesj995 <mrjam7@gmail.com>
2021-08-10 19:28:05 -0500

    added order_details model

---
+++ models/order_details.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class OrderDetails extends Model {}
+
+OrderDetails.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    total: {
+      type: DataTypes.TEXT
+    },
+    payment_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'payment',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'OrderDetails',
+  }
+);
+
+module.exports = OrderDetails;
