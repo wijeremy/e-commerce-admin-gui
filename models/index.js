89f7dc7cead4789b1ae2207d431f5aaf20d47e58
wijeremy <jeremydavwilliams@gmail.com>
2021-08-12 18:41:43 -0500

    fixed conflicts

--- models/index.js
+++ models/index.js
@@ -10,88 +10,75 @@ const UserShoppingSession = require('./user_shopping_session');
 const User = require('./User');
 
 Category.hasMany(Product, {
-	foreignKey: 'category_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 Product.belongsTo(Category, {
-	foreignKey: 'category_id',
+  foreignKey: 'category_id',
 });
 
 Inventory.hasOne(Product, {
-	foreignKey: 'inventory_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'inventory_id',
+  onDelete: 'CASCADE',
 });
 
 Product.belongsTo(Inventory, {
-	foreignKey: 'inventory_id',
+  foreignKey: 'inventory_id',
 });
 
 Product.hasMany(OrderItems, {
-	foreignKey: 'product_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
 });
 
 OrderItems.belongsTo(Product, {
-	foreignKey: 'product_id',
+  foreignKey: 'product_id',
 });
 
 Product.hasMany(CartItem, {
-	foreignKey: 'product_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
 });
 
 CartItem.belongsTo(Product, {
-	foreignKey: 'product_id',
+  foreignKey: 'product_id',
 });
 
 Payment.hasOne(OrderDetails, {
-	foreignKey: 'payment_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'payment_id',
+  onDelete: 'CASCADE',
 });
 
 OrderDetails.belongsTo(Payment, {
-	foreignKey: 'payment_id',
+  foreignKey: 'payment_id',
 });
 
 User.hasOne(UserShoppingSession, {
-	foreignKey: 'user_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 UserShoppingSession.belongsTo(User, {
-	foreignKey: 'user_id',
+  foreignKey: 'user_id',
 });
 
 User.hasMany(Payment, {
-	foreignKey: 'user_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 Payment.belongsTo(User, {
-	foreignKey: 'user_id',
+  foreignKey: 'user_id',
 });
 
 UserShoppingSession.hasMany(CartItem, {
-	foreignKey: 'session_id',
-	onDelete: 'CASCADE',
+  foreignKey: 'session_id',
+  onDelete: 'CASCADE',
 });
 
 CartItem.belongsTo(UserShoppingSession, {
-	foreignKey: 'session_id',
-});
-
-OrderDetails.hasMany(OrderItems, {
-	foreignKey: 'order_id',
-	onDelete: 'CASCADE',
-});
-
-OrderItems.belongsTo(OrderDetails, {
-	foreignKey: 'order_id',
-});
-
-OrderItems.belongsTo(OrderDetails, {
-  foreignKey: 'order_id',
+  foreignKey: 'session_id',
 });
 
 OrderDetails.hasMany(OrderItems, {
@@ -99,13 +86,17 @@ OrderDetails.hasMany(OrderItems, {
   onDelete: 'CASCADE',
 });
 
+OrderItems.belongsTo(OrderDetails, {
+  foreignKey: 'order_id',
+});
+
 module.exports = {
-	Category,
-	Inventory,
-	OrderDetails,
-	Payment,
-	Product,
-	UserCartItem,
-	UserShoppingSession,
-	User,
+  Category,
+  Inventory,
+  OrderDetails,
+  Payment,
+  Product,
+  UserCartItem,
+  UserShoppingSession,
+  User,
 };
