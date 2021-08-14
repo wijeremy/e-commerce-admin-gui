6ff7e180e8064d4b1f2b73baebf8b8fca9219d2b
javier vilchis <javivilchis@gmail.com>
2021-08-14 12:42:29 -0500

    adding the structure for admin / home

---
+++ controllers/admin/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const productRoutes = require('./productRoutes');
+
+
+router.use('/users',userRoutes);
+router.use('/product', productRoutes);
+
+module.exports = router;
