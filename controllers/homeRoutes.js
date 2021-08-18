065f0ff194ecab0fb1de5413939f6a8047005dbd
wijeremy <jeremydavwilliams@gmail.com>
2021-08-17 19:48:45 -0500

    got tiles of products to display on home screen

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,32 @@ const verifyToken = require('../utils/auth');
 
 //need to get all of the products from database
 router.get('/', async (req, res) => {
-<<<<<<< HEAD
-     res.render('home', { layout: 'user' });
+     try {
+          // Get all projects and JOIN with user data
+          const productData = await Product.findAll({});
+
+          // Serialize data so the template can read it
+          const products = productData.map((product) =>
+               product.get({ plain: true })
+          );
+
+          // Pass serialized data and session flag into template
+          res.render('home', {
+               products,
+               layout: 'user',
+          });
+     } catch (err) {
+          res.status(500).json(err);
+     }
+});
+
+router.get('/login', (req, res) => {
+     // If the user is already logged in, redirect the request to another route
+     if (verifyToken) {
+          res.redirect('/');
+          return;
+     }
+     res.render('login');
 });
 
 //need to wire up the product details page
@@ -26,59 +50,4 @@ router.get('/product/:id', async (req, res) => {
      res.render('product', { layout: 'user' });
 });
 
-router.get('/login', (req, res) => {
-     // If a session exists, redirect the request to the homepage
-     if (req.session.logged_in) {
-          res.redirect('/');
-          return;
-     }
-
-     res.render('login');
-});
-=======
-  try {
-    // Get all projects and JOIN with user data
-    const productData = await Product.findAll({});
-
-    // Serialize data so the template can read it
-    const products = productData.map((product) => product.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('home', {
-      ...products,
-      layout: 'user',
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (verifyToken) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
-});
-
-//need to wire up the product details page
-router.get('/product/:id', async (req, res) => {
-  const guitar = [
-    {
-      name: 'A Classic, Reborn',
-      description:
-        "The new Les Paul Standard returns to the classic design that made it relevant, played and loved -- shaping sound across generations and genres of music. It pays tribute to Gibson's Golden Era of innovation and brings authenticity back to life. The Les Paul Standard 50's has a solid mahogany body with a maple top, a rounded 50's-style mahogany neck with a rosewood fingerboard and trapezoid inlays. It's equipped with an ABR-1, the classic-style Tune-O-Matic bridge, aluminum stop bar tailpiece, vintage deluxe tuners with keystone buttons, and aged gold tophat knobs. The calibrated Burstbucker 1 (neck) and Burstbucker 2 (bridge) pickups are loaded with AlNiCo II magnets, audio taper potentiometers and orange drop capacitors.",
-      price: '$3,500',
-      imgage:
-        'https://cdn.pixabay.com/photo/2017/03/12/16/55/electric-guitar-2137492_1280.jpg',
-    },
-  ];
-
-  //const chosenGuitar = guitar.get({ plain: true });
-  //res.json(guitar);
-  res.render('product', { layout: 'user' });
-});
-
->>>>>>> 35c75624f8550294925d4353475ce82117161677
 module.exports = router;
